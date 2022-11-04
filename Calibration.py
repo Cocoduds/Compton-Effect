6188a2ab42e166b5332bc3b12fac66f069c2a552
Cocoduds <85304264+Cocoduds@users.noreply.github.com>
2022-11-04 14:56:37 +0000

    double gaussian fitting

---
+++ Calibration.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  4 14:06:39 2022
+
+@author: dudle
+"""
+
+
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+plt.close("all")
+
+Am=np.loadtxt("Am_251cal.csv", delimiter = ',', encoding='utf-8-sig')
+Cs=np.loadtxt("Cs_137cal.csv", delimiter = ',', encoding='utf-8-sig')
+Co=np.loadtxt("Co_57cal.csv", delimiter = ',', encoding='utf-8-sig')
+
+
+expected = [59.6, 662, 122]
+peaks = []
+
+#x is energy
+#y is counts
+
+def fit(x,a,mu,sigma):
+    y_gaus=a*np.exp(-(x-mu)**2 / (2*sigma**2))
+    return y_gaus 
+
+
+
+def FindMaxima(numbers):
+    maxima = []
+    length = len(numbers)
+    if length >= 2:
+        if numbers[0] > numbers[1]:
+            maxima.append(numbers[0])
+        if length > 3:
+            for i in range(1, length-1):    
+                if numbers[i] > numbers[i-1] and numbers[i] > numbers[i+1]:
+                    maxima.append(numbers[i])
+        if numbers[length-1] > numbers[length-2]:     
+            maxima.append(numbers[length-1])       
+    return maxima
+
+
+x = np.linspace(0,511,512)
+
+
+xloads= np.linspace(0,511,5110)
+
+
+plt.figure(1)
+mu=np.mean(x)
+sigma= np.std(x)
+a= 1/(sigma*np.sqrt(2*np.pi))
+initial_guess=[a,mu,sigma]
+gaus,gaus_cov = curve_fit(fit,x,Am,initial_guess,maxfev=5000)
+plt.plot(x,Am)
+y=fit(xloads,*gaus).tolist()
+plt.plot(xloads,y)
+peaks.append(xloads[y.index(max(y))])
+plt.show()
+
+
+
+Cs_res=[]
+xres=[]
+for i in range(350,511):
+    Cs_res.append(Cs[i])
+    xres.append(x[i])
+
+xloads= np.linspace(350,511,(511-350)*10)
+plt.figure(2)
+gaus,gaus_cov = curve_fit(fit,xres,Cs_res,initial_guess,maxfev=5000)
+plt.plot(x,Cs)
+y=fit(xloads,*gaus).tolist()
+plt.plot(xloads,y)
+peaks.append(xloads[y.index(max(y))])
+plt.show()
+
+xloads= np.linspace(0,511,5110)
+plt.figure(3)
+gaus,gaus_cov = curve_fit(fit,x,Co,initial_guess,maxfev=5000)
+plt.plot(x,Co)
+y=fit(xloads,*gaus).tolist()
+plt.plot(xloads,y)
+peaks.append(xloads[y.index(max(y))])
+plt.show()
+
+
+plt.figure(4)
+plt.scatter(peaks,expected)
+fit,cov = np.polyfit(peaks,expected,1,cov=True)
+sig = np.sqrt(cov[0,0])
+
+p1=np.poly1d(fit)
+print('Fit polynomial (for corr. fac.)')
+print(p1)
+plt.plot(expected,p1(expected))
---
+++ ComptonEffect.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  4 14:16:00 2022
+
+@author: dudle
+"""
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from Calibration import p1 as Calibrate
+from scipy.signal import savgol_filter
+
+plt.close("all")
+data=np.genfromtxt('Cs-137_2.txt', skip_header = 5, skip_footer = 1, autostrip = True)
+
+def _1gaussian(x, amp1, cen1, sigma1):
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2)))
+
+
+def _2gaussian(x, amp1,cen1,sigma1, amp2,cen2,sigma2):
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2))) + \
+            amp2*(1/(sigma2*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen2)/sigma2)**2)))
+
+
+data[:,0] = np.linspace(0,510,511)
+bins = data[:,0]
+deg=[30,60,70,80,90]              
+for i in range(1,6):
+    #seperate figures
+    plt.figure(i)
+    # noise reduction
+    data_smoothed = savgol_filter(data[:,i], 51, 2)
+    
+    # single gaussian fits
+    # popt_gauss, pcov_gauss = curve_fit(_1gaussian, bins, data[:,i], p0=[60, 150, 10])
+    # perr_gauss = np.sqrt(np.diag(pcov_gauss))
+    # plt.plot(bins, _1gaussian(bins, *popt_gauss),label=str(deg[1-i])+" deg")
+    
+    # two gaussian fits
+    popt_2gauss, pcov_2gauss = curve_fit(_2gaussian, bins, data[:,i], p0=[60, 20, 10, 60, 150, 10])
+    perr_2gauss = np.sqrt(np.diag(pcov_2gauss))
+    pars_1 = popt_2gauss[0:3]
+    pars_2 = popt_2gauss[3:6]
+    gauss_peak_1 = _1gaussian(bins, *pars_1)
+    gauss_peak_2 = _1gaussian(bins, *pars_2)
+    plt.plot(bins, _2gaussian(bins, *popt_2gauss),label=str(deg[1-i])+" deg")
+    
+    
+    plt.plot(bins, data[:,i],label=str(deg[1-i])+" deg")
+
+plt.xlabel('Energy (KeV)')
+plt.ylabel('Counts')
+plt.legend()
+ax = plt.gca()
+ax.set_ylim([0, 150])
